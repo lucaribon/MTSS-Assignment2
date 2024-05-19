65993d27b0a540058d8489614a8005ffd91240fb
lucaribon <lucaribon0203@gmail.com>
2024-05-20 00:28:00 +0200

    Sviluppo funzione IntegerToRoman.convert(); closes #8

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -7,7 +7,50 @@ package it.unipd.mtss;
 
 public class IntegerToRoman {
     public static String convert(int number) {
-        // TODO
-        return null;
+        // if number is below 1 or above 3999, return an empty string
+        if(number < 1 || number>3999) {
+            return "";
+        }
+
+        StringBuilder romanNumber = new StringBuilder();
+
+        String[] romanChars = {
+                "M",
+                "CM",
+                "D",
+                "CD",
+                "C",
+                "XC",
+                "L",
+                "XL",
+                "X",
+                "IX",
+                "V",
+                "IV",
+                "I"
+        };
+        int[] romanValues = {
+                1000,
+                900,
+                500,
+                400,
+                100,
+                90,
+                50,
+                40,
+                10,
+                9,
+                5,
+                4,
+                1
+        };
+
+        for (int i = 0; i < romanChars.length; i++) {
+            while (number >= romanValues[i]) {
+                romanNumber.append(romanChars[i]);
+                number -= romanValues[i];
+            }
+        }
+        return romanNumber.toString();
     }
 }
